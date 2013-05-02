d2eeea0255a4cf465cb5bd4a536924351c3938c1
Nicolas Roy <niroyb@gmail.com>
2013-05-02 03:17:22 -0400

    now works with uneven output node levels
    removed self.top

--- retropropagation.py
+++ retropropagation.py
@@ -15,14 +15,14 @@ class ReseauNeurones():
     def __init__(self, links):
         '''Creates a network based on the given topology.
         links : [(node1, node2), ..., (node3, node4)]'''
-        self.weight = defaultdict(dict) # weights between two nodes
-        self.children = defaultdict(list) #Noeuds fils
+        self.weight = defaultdict(dict)  # weights between two nodes
+        self.children = defaultdict(list)
 
         for link in links:
             self.__addSynapse(*link)
         self.__initLayers()
         
-    def __addSynapse(self, fromNeurone, toNeurone, intialWeight=0.5):
+    def __addSynapse(self, fromNeurone, toNeurone, intialWeight = 0.5):
         '''Adds a directional link between two nodes/neurons'''
         self.weight[fromNeurone][toNeurone] = intialWeight
         self.children[toNeurone].append(fromNeurone)
@@ -36,33 +36,33 @@ class ReseauNeurones():
             In = deepcopy(A)
             for level in xrange(2, self.M + 1):
                 for i in self.layer[level]:
-                    In[i] = sum(self.weight[j][i]*A[j] for j in self.children[i])
+                    In[i] = sum(self.weight[j][i] * A[j] for j in self.children[i])
                     A[i] = g(In[i])
             
             delta = {}
             # Calculate delta of output layer
-            for i in self.top:
-                delta[i] = gPrime(In[i])*(y[i] - A[i])
+            for i in self.layer[self.M]:
+                delta[i] = gPrime(In[i]) * (y[i] - A[i])
                 
             newGraph = deepcopy(self.weight)
             
             # Propagate deltas backward from output to input layer
             
-            #For all layers but the top one
-            for level in xrange(self.M-1, 1, -1):
+            # For all layers but the outputNodes one
+            for level in xrange(self.M - 1, 1, -1):
                 # For nodes in current layer
-                for j in self.layer[level]: # j is current node
+                for j in self.layer[level]:  # j is current node
                     # Calculate the error based on weights to higher layers
-                    delta[j] = gPrime(In[j])*sum(self.weight[j][i]*delta[i] for i in self.weight[j])
+                    delta[j] = gPrime(In[j]) * sum(self.weight[j][i] * delta[i] for i in self.weight[j])
             
             # Update every weight in the network using deltas
             for j in self.weight:
                 for i in self.weight[j]:
-                    newGraph[j][i] += alpha*A[j]*delta[i]
+                    newGraph[j][i] += alpha * A[j] * delta[i]
             self.weight = newGraph
-        #print A
-        #print delta
-        #print self.weight
+        # print A
+        # print delta
+        # print self.weight
     
     def __initLayers(self):
         '''Finds the neurones per layer and the number of layers'''
@@ -72,17 +72,21 @@ class ReseauNeurones():
             fromN.add(n1)
             for n2 in self.weight[n1]:
                 toN.add(n2)
-        self.top = toN - fromN #Top layer
+        outputNodes = toN - fromN  # Top layer
         # mid = fromN & toN #Hidden neurons
-        bot = fromN - toN  # Entry layer
+        inputNodes = fromN - toN  # bottom layer
         
         self.layer = {}
         level = 1
-        levelNodes = bot
+        levelNodes = inputNodes
         while len(levelNodes):
             self.layer[level] = levelNodes
-            levelNodes = set(n2 for n1 in levelNodes for n2 in self.weight[n1])
+            levelNodes = set(n2 for n1 in levelNodes
+                             for n2 in self.weight[n1]
+                             if n2 not in outputNodes)
             level += 1
+
+        self.layer[level] = outputNodes
         self.M = len(self.layer)
         print self.layer, self.M
 

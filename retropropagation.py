bdce1ee2987040062e9030b515429b98625c4f96
Nicolas Roy <niroyb@gmail.com>
2013-05-02 02:19:25 -0400

    refactor variable names and init

--- retropropagation.py
+++ retropropagation.py
@@ -12,26 +12,31 @@ def gPrime(val):
     return g(val) * (1.0 - g(val))
 
 class ReseauNeurones():
-    def __init__(self):
-        self.graph = defaultdict(dict)
-        self.back = defaultdict(list) #Noeuds fils
-        
-    def addSynapse(self, fromNeurone, toNeurone, intialWeight=0.5):
-        '''Adds a link between two neurones'''
-        self.graph[fromNeurone][toNeurone] = intialWeight
-        self.back[toNeurone].append(fromNeurone)
+    def __init__(self, links):
+        '''Creates a network based on the given topology.
+        links : [(node1, node2), ..., (node3, node4)]'''
+        self.weight = defaultdict(dict) # weights between two nodes
+        self.children = defaultdict(list) #Noeuds fils
 
-    def epoque(self, examples, alpha):
-        '''Do one iteration of the retropropagation algorithm
-        with all the given examples'''
+        for link in links:
+            self.__addSynapse(*link)
         self.__initLayers()
         
-        # For input, output in given examples
+    def __addSynapse(self, fromNeurone, toNeurone, intialWeight=0.5):
+        '''Adds a directional link between two nodes/neurons'''
+        self.weight[fromNeurone][toNeurone] = intialWeight
+        self.children[toNeurone].append(fromNeurone)
+
+    def backPropagationLearning(self, examples, alpha):
+        '''Do one iteration of the retropropagation algorithm
+        with all the given examples'''
+        
+        # For (input, output) in given examples
         for A, y in examples:
             In = deepcopy(A)
-            for L in xrange(2, self.M + 1):
-                for i in self.couche[L]:
-                    In[i] = sum(self.graph[j][i]*A[j] for j in self.back[i])
+            for level in xrange(2, self.M + 1):
+                for i in self.layer[level]:
+                    In[i] = sum(self.weight[j][i]*A[j] for j in self.children[i])
                     A[i] = g(In[i])
             
             delta = {}
@@ -39,45 +44,45 @@ class ReseauNeurones():
             for i in self.top:
                 delta[i] = gPrime(In[i])*(y[i] - A[i])
                 
-            newGraph = deepcopy(self.graph)
+            newGraph = deepcopy(self.weight)
             
             # Propagate error correction to layers below
             
             #For all layers but the top one
-            for L in xrange(self.M-1, 0, -1):
+            for level in xrange(self.M-1, 0, -1):
                 # For nodes in current layer
-                for j in self.couche[L]: # j is current node
+                for j in self.layer[level]: # j is current node
                     # Calculate the error based on weights to higher layers
-                    delta[j] = gPrime(In[j])*sum(self.graph[j][i]*delta[i] for i in self.graph[j])
+                    delta[j] = gPrime(In[j])*sum(self.weight[j][i]*delta[i] for i in self.weight[j])
                     # Assign corrected weights for links with higher layer
-                    for i in self.graph[j]: # i is a parent of j
+                    for i in self.weight[j]: # i is a parent of j
                         newGraph[j][i] += alpha*A[j]*delta[i]
-            self.graph = newGraph
+            self.weight = newGraph
         #print A
         #print delta
-        #print self.graph
+        #print self.weight
     
     def __initLayers(self):
         '''Finds the neurones per layer and the number of layers'''
         fromN = set()
         toN = set()
-        for n1 in self.graph:
+        for n1 in self.weight:
             fromN.add(n1)
-            for n2 in self.graph[n1]:
+            for n2 in self.weight[n1]:
                 toN.add(n2)
         self.top = toN - fromN #Top layer
         # mid = fromN & toN #Hidden neurons
         bot = fromN - toN  # Entry layer
         
-        self.couche = {}
+        self.layer = {}
         level = 1
         levelNodes = bot
         while len(levelNodes):
-            self.couche[level] = levelNodes
-            levelNodes = set(n2 for n1 in levelNodes for n2 in self.graph[n1])
+            self.layer[level] = levelNodes
+            levelNodes = set(n2 for n1 in levelNodes for n2 in self.weight[n1])
             level += 1
-        self.M = len(self.couche)
-        print self.couche, self.M
+        self.M = len(self.layer)
+        print self.layer, self.M
 
 links = [(0 , 4, 1),
          (1, 4, 0.5),
@@ -90,17 +95,15 @@ links = [(0 , 4, 1),
          (5, 6, 0.5),
          (5, 7, 1)]
 
-r = ReseauNeurones()
-for link in links:
-    r.addSynapse(*link)
+r = ReseauNeurones(links)
 
 exemples = [({0:0.8, 1:0.5, 2:0.5, 3:0.4}, {6:0.2, 7:0.5})]  # list of (inputs, output)
 alpha = 1.0
 
-r.epoque(exemples, alpha)
+r.backPropagationLearning(exemples, alpha)
 
 print "New synapse weights:"
-for n1, n2W in r.graph.items():
+for n1, n2W in r.weight.items():
     if len(n2W):
         for n2, W in n2W.items():
             print '{} -> {} = {:0.3}'.format(n1, n2, W)
